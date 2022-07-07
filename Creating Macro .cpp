609a3e7f66e94d149073460fb10a0e8b9b2701e9
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-07-07 22:53:03 +0530

    Add files via upload

---
+++ Creating Macro .cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#define pi 3.14
+// using macro for find the area
+
+
+int main()
+{
+    int r;
+    cout<<"enter the radius :"<<endl;
+    cin>>r;
+    
+    int area = pi*r*r;
+    cout<<"area of the given radius circle is :"<<area<<endl;
+    
+
+    return 0;
+}
