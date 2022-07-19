f6fbb9f66b1b78813bf610814cae6cedfbe15086
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-07-19 23:37:27 +0530

    Add files via upload

---
+++ Remove Duplicates from SortedList.cpp
@@ -0,0 +1,20 @@
+/*
+Remove Duplicates from Sorted List
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+*/
+
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* cur = head;
+        while(cur) {
+        	while(cur->next && cur->val == cur->next->val) {
+        		cur->next = cur->next->next;
+        	}
+        	cur = cur->next;
+        }
+        return head;
+    }
+};
