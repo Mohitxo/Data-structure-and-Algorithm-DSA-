18fd8577ca0cf355fa6e8d48fb80a693e813f28f
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-07-27 22:14:49 +0530

    Add files via upload

---
+++ queue stl.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue<int>q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    cout<<"front of queue is :"<<q.front()<<endl;
+    q.pop();
+    cout<<"front of queue is :"<<q.front()<<endl;
+    if(q.empty())
+    cout<<"queue is empty"<<endl;
+    else
+    cout<<"queue is not empty"<<endl;
+    
+    cout<<"size of queue is :"<<q.size()<<endl;
+
+    return 0;
+}
