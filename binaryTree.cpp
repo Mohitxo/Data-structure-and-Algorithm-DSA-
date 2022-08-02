586581da51593be0f81e840177bfa074c16bea06
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-08-02 23:26:26 +0530

    Add files via upload

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -34,36 +34,7 @@ node* buildTree(node* root) {
 
 }
 
-void levelOrderTraversal(node* root) {
-    queue<node*> q;
-    q.push(root);
-    q.push(NULL);
 
-    while(!q.empty()) {
-        node* temp = q.front();
-        q.pop();
-
-        if(temp == NULL) { 
-            //purana level complete traverse ho chuka hai
-            cout << endl;
-            if(!q.empty()) { 
-                //queue still has some child ndoes
-                q.push(NULL);
-            }  
-        }
-        else{
-            cout << temp -> data << " ";
-            if(temp ->left) {
-                q.push(temp ->left);
-            }
-
-            if(temp ->right) {
-                q.push(temp ->right);
-            }
-        }
-    }
-
-}
 
 void inorder(node* root) {
     //base case
@@ -100,40 +71,6 @@ void postorder(node* root) {
     cout << root-> data << " ";
 
 }
-
-void buildFromLevelOrder(node* &root) {
-    queue<node*> q;
-
-    cout << "Enter data for root" << endl;
-    int data ;
-    cin >> data;
-    root = new node(data);
-    
-    q.push(root);
-
-    while(!q.empty()) {
-        node* temp = q.front();
-        q.pop();
-
-        cout << "Enter left node for: " << temp->data << endl;
-        int leftData;
-        cin >> leftData;
-
-        if(leftData != -1) {
-            temp -> left = new node(leftData);
-            q.push(temp->left);
-        }
-
-        cout << "Enter right node for: " << temp->data << endl;
-        int rightData;
-        cin >> rightData;
-
-        if(rightData != -1) {
-            temp -> right = new node(rightData);
-            q.push(temp->right);
-        }
-    }
- }
 
 
 int main() {
