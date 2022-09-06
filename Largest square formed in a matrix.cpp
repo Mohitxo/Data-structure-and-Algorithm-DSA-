96520416d30651a41f06a93546b9f43da519d706
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-09-06 22:42:26 +0530

    Add files via upload

---
+++ Largest square formed in a matrix.cpp
@@ -0,0 +1,22 @@
+    int solveMem(vector<vector<int>>&mat,int i,int j int &maxi,vector<vector<int>> &dp){
+        if(i >= mat.size() || j >= mat[0].size())
+            return 0;
+    
+        if((dp[i][j] != -1))
+            return dp[i][j];
+        
+        int right = solveMem(mat,i,j+1,maxi,dp);
+        int diagnol = solveMem(mat,i+1,j+1,maxi,dp);
+        int down = solveMem(mat,i+1,j,maxi,dp);
+        
+        if(mat[i][j] == 1){
+            dp[i][j] = 1+ min(right,min(diagnol,down));
+            maxi = max(maxi,dp[i][j]);
+            return dp[i][j];
+        }
+        else
+        {
+            return dp[i][j] = 0;
+        }
+}
+   
